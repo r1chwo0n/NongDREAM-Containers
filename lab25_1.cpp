f909c18d6eda1c6185cd6c96e8e8481e7e3f006d
r1chwo0n <woonzy09@gmail.com>
2023-03-10 17:56:51 +0700

    completed

--- lab25_1.cpp
+++ lab25_1.cpp
@@ -63,4 +63,18 @@ void List::append(int d){
 	size++;
 }
 
-//Write List::remove() here
+void List::remove(int idx){ 
+    size--;
+    Node *current = root;
+    if(idx == 0){
+        root = current -> next;
+        delete current;
+    }else{
+        for(int i = 0; i < idx-1;i++){
+    		current = current->next;
+    	}
+    	Node *p = current -> next;
+    	current -> next = p -> next;
+    	delete p;
+    }
+}
--- lab25_2.cpp
+++ lab25_2.cpp
@@ -23,7 +23,34 @@ int main(){
 	loc = find(line_up.begin(),line_up.end(),"Bob");
 	line_up.insert(loc,"Oscar");
 	
-	//Write your code here
+	line_up.push_back("Luffy");
+	line_up.push_back("Sanji");
+	line_up.push_back("Nami");
+	
+	loc = find(line_up.begin(),line_up.end(),"Alice");
+	line_up.erase(loc);
+	loc = find(line_up.begin(),line_up.end(),"Oscar");
+	line_up.erase(loc);
+	
+	loc = find(line_up.begin(),line_up.end(),"Luffy");
+	loc++;
+	line_up.insert(loc,"Narutu");
+	
+	line_up.push_front("Prayath");
+	
+	loc = find(line_up.begin(),line_up.end(),"Prayath");
+	loc++;
+	line_up.insert(loc,"Tony");
+	
+	loc = find(line_up.begin(),line_up.end(),"Bob");
+	line_up.erase(loc);
+	
+	loc = find(line_up.begin(),line_up.end(),"Prayath");
+	line_up.erase(loc);
+	loc = find(line_up.begin(),line_up.end(),"Tony");
+	line_up.erase(loc);
+	loc = find(line_up.begin(),line_up.end(),"Luffy");
+	line_up.erase(loc);
 	
 	printList(line_up);
 		
